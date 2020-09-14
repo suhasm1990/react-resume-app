2baa59289236d80d11b5fe86a4eaab9f8edd5eaa
suhasm1990 <suhu.90@gmail.com>
2020-09-14 14:18:12 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-edds8c
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-edds8c)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Suhas!</h1>
+      <p>Welcome to your New App!</p>
+    </div>
+  );
+}
